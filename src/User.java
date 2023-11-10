596122040483084b4188f49ab5724661df5b286d
ruiallves <ruisteampt5@gmail.com>
2023-11-10 15:24:22 +0000

    update user(add interface)

--- src/User.java
+++ src/User.java
@@ -2,15 +2,14 @@ package src;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class User {
     public static void main(String[] args) {
         try {
-
             Socket socket = new Socket("localhost", 9090);
 
             Thread tcpThread = new Thread(new TCPThread(socket));
@@ -19,51 +18,114 @@ public class User {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     static class TCPThread implements Runnable {
         private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+        private Scanner scanner;
 
         public TCPThread(Socket socket) {
             this.socket = socket;
+            this.scanner = new Scanner(System.in);
+
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
         public void run() {
+            int teste = 0;
             try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+                while (true) {
+                    showMenu();
 
-                Thread serverReader = new Thread(() -> {
-                    try {
-                        String receivedMessage;
-                        while ((receivedMessage = in.readLine()) != null) {
-                            System.out.println(receivedMessage);
-                            if (receivedMessage.isEmpty()) {
-                                break;
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    System.out.println("Opção: ");
+                    int choice = scanner.nextInt();
+                    switch (choice) {
+                        case 1:
+                            registerUser();
+                            teste++;
+                            break;
+                        case 2:
+                            loginUser();
+                            teste++;
+                            break;
+                        case 3:
+                            executeTask();
+                            break;
+                        case 4:
+                            System.out.println("Adeus!");
+                            System.exit(0);
+                        default:
+                            System.out.println("Opção inválida. Tente novamente.");
                     }
-                });
-                serverReader.start();
-
-                String userInput;
-                while ((userInput = consoleReader.readLine()) != null) {
-                    out.println(userInput);
                 }
-
-                serverReader.join();
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
+        private void showMenu() {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Registrar usuário");
+            System.out.println("2. Fazer login");
+            System.out.println("3. Executar tarefa");
+            System.out.println("4. Sair");
+        }
+
+        private void registerUser() {
+            System.out.println("Insira o nome de usuário:");
+            String username = scanner.next();
+            System.out.println("Insira a senha:");
+            String password = scanner.next();
+
+            out.println("REGISTER " + username + " " + password);
+            out.flush();
+
+            printServerResponse();
+        }
+
+        private void loginUser() {
+            System.out.println("Insira o nome de usuário:");
+            String username = scanner.next();
+            System.out.println("Insira a senha:");
+            String password = scanner.next();
+
+            out.println("LOGIN " + username + " " + password);
+            out.flush();
+
+            printServerResponse();
+        }
+
+        private void executeTask() {
+            System.out.println("Insira o código da tarefa:");
+            String taskCode = scanner.next();
+
+            out.println("EXECUTE " + taskCode);
+            out.flush();
+
+            printServerResponse();
+            printServerResponse();
+        }
+
+        private void printServerResponse() {
+            try {
+                String receivedMessage;
+                while ((receivedMessage = in.readLine()) != null) {
+                    System.out.println(receivedMessage);
+                    break;
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
-
 }
